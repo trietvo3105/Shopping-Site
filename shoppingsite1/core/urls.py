2e0cb1fcbf4ac3fcfb84162b445c5900f52b310a
quyennguyenmanh2112 <47165806+quyennguyenmanh2112@users.noreply.github.com>
2020-06-07 11:18:04 +0700

    create footer.html, navbar.html, index.html and add css + picture for index.html

--- shoppingsite1/core/urls.py
+++ shoppingsite1/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import DangNhap, UserView, dang_ky
+from .views import DangNhap, UserView, dang_ky, HomePage
 
 app_name = 'core'
 urlpatterns = [
     path('login/', DangNhap.as_view(), name='login'),
     path('register/', dang_ky, name='register'),
-    path('userview/', UserView.as_view(), name='user-view')
+    path('userview/', UserView.as_view(), name='user-view'),
+    path('', HomePage.as_view(), name='index'),
 ]
