8658c5b1c099c4c50069a35650776b8b1036406c
Triet Vo <trietvo315@gmail.com>
2020-06-23 07:52:07 +0700

    update (gần) full giỏ hàng (chưa có save)

--- shoppingsite1/core/urls.py
+++ shoppingsite1/core/urls.py
@@ -19,7 +19,15 @@ urlpatterns = [
     path('nghethuat/', NgheThuat.as_view(), name='nghethuat'),
     path('ngoaivan/', NgoaiVan.as_view(), name='ngoaivan'),
     path('cart/cart_add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
     path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
+    path('cart/item_clear/<int:id>/',
+         views.item_clear, name='item_clear'),
+    path('cart/item_increment/<int:id>/',
+         views.item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',
+         views.item_decrement, name='item_decrement'),
+
 ]
 
 if settings.DEBUG:
--- shoppingsite1/core/views.py
+++ shoppingsite1/core/views.py
@@ -170,5 +170,36 @@ def cart_add(request, id):
 
 
 @login_required(login_url='/login/')
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect('core:cart_detail')
+
+
+@login_required(login_url='/login/')
 def cart_detail(request):
     return render(request, 'cart/cart_detail.html')
+
+
+@login_required(login_url='/login/')
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Sach.objects.get(id=id)
+    cart.remove(product)
+    return redirect('core:cart_detail')
+
+
+@login_required(login_url='/login/')
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Sach.objects.get(id=id)
+    cart.add(product=product)
+    return redirect('core:cart_detail')
+
+
+@login_required(login_url='/login/')
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Sach.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect('core:cart_detail')
